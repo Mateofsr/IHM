8ef43e2898eaf6be08ef4dda62483fc2ab2ba5e8
Chaipo-arch <81694579+Chaipo-arch@users.noreply.github.com>
2022-06-01 14:16:04 +0200

    Update EspaceTampon.java

--- EspaceTampon.java
+++ EspaceTampon.java
@@ -4,12 +4,13 @@
  */
 package application;
 
-import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Reader;
+import java.util.Scanner;
+
 
 /** TODO commenter la responsabilité de cette classe
  * @author 33766
@@ -17,7 +18,23 @@ import java.io.Reader;
  */
 public class EspaceTampon {
 
-    /** TODO commenter le rôle de cette méthode (SRP)
+	/**création d'un nom de fichier par défaut*/
+	public static String nomFichier="nomParDéfaut";
+	/**création d'un fichier par défaut*/
+	public static File fichier = new File(nomFichier+".txt");
+	/***/
+	public static String aEcrire;
+	 /** 
+     * permet de supprimer le fichier texte courant
+     * 
+     */
+    public static void suppressionFichier() {
+        File fichier = new File(nomFichier+".txt"); 
+        fichier.delete();
+    }
+	
+    /** 
+     * permet de supprimer le fichier texte avec le nom mentionné en paramétre
      * @param nomFichier
      */
     public static void suppressionFichier(String nomFichier) {
@@ -26,11 +43,12 @@ public class EspaceTampon {
         if(fichier.exists()) { 
              fichier.delete();
         } else { 
-            System.out.println("le fichier n'existe pas"); 
+            System.out.println("le fichier n'existe pas ou le nom mentionné n'est pas le bon"); 
         } 
     }
-    
-    /** TODO commenter le rôle de cette méthode (SRP)
+   
+    /** 
+     * permet de créer un nouveau fichier texte avec comme nom spécifié en paramétre
      * @param nomFichier
      * @throws IOException
      */
@@ -39,26 +57,120 @@ public class EspaceTampon {
         File fichier = new File(nomFichier+".txt");
         while(fichier.exists()) {
             numero++;
-            fichier = new File(nomFichier+numero+"txt");
+            fichier = new File(nomFichier+numero+".txt");
             fichier.renameTo(fichier);
         } 
+        fichier.createNewFile();
+        ouvrirFichier(nomFichier);
     }
 
-    /** TODO commenter le rôle de cette méthode (SRP)
+    /** 
+     * methode qui permette d'ouvrir et d'afficher le fichier texte dont le nom est spécifié en 
+     * paramétre sur l'interface graphique
      * @param nomFichier
      * @throws IOException 
      */
     public static void ouvrirFichier(String nomFichier) throws IOException {
-        File fichier = new File(nomFichier+"txt");
+        fichier = new File(nomFichier+".txt");
+        
         if(fichier.exists()) {
-            FileReader lire = new FileReader(nomFichier+".txt");
-            BufferedReader lireFichier = new BufferedReader(lire);
-            while(lireFichier.readLine()!=null) {
-                //afficher dans l'interface le texte
-            }
+            lireFichierEntier(fichier);
+
         }else {
             throw new FileNotFoundException("fichier introuvable");
         }
+    }
+    /** 
+     * methode qui permette d'ouvrir et d'afficher le fichier texte courant
+     * @throws IOException 
+     */
+    public static void ouvrirFichier() throws IOException {
+        File fichier = new File(nomFichier+".txt");
+        
+        if(fichier.exists()) {
+            lireFichierEntier(fichier);
+        }else {
+            throw new FileNotFoundException("fichier introuvable");
+        }
+    }
+    /**
+     * lit le fichier mentionné en paramétre en entier
+     * @param fichier
+     * @throws FileNotFoundException
+     */
+    public static void lireFichierEntier(File fichier) throws FileNotFoundException {
+    	Scanner lecture = new Scanner(fichier);
+    	while(lecture.hasNextLine()) {
+    		System.out.println( lecture.nextLine()); 
+    		//TODO changer le sysout pour qu'il apparaisse dans l'interface plutot que dans la console
+    	}
+    }
+    /**
+     * lit le fichier courant en entier
+     * @param fichier
+     * @throws FileNotFoundException
+     */
+    public static void lireFichierEntier() throws FileNotFoundException {
+    	Scanner lecture = new Scanner(fichier);
+    	while(lecture.hasNextLine()) {
+    		System.out.println( lecture.nextLine()); 
+    		//TODO changer le sysout pour qu'il apparaisse dans l'interface plutot que dans la console
+    	}
+    }
+    /**
+     * 
+     * @param ligne
+     * @return
+     */
+    public static boolean ligneExiste(int ligne) {
+		return false;//bouchon
+    	//TODO ecrire la methode
+    }
+    /**
+     * supprime une ligne mentionné du fichier courant
+     * @param fichier
+     * @param ligne
+     * @throws IOException 
+     */
+    public static void suppression1Ligne(int ligne) throws IOException {
+    	Scanner lecture = new Scanner(fichier);
+    	File fichierbis = new File(nomFichier+"bis.txt");
+    	
+    	if(!ligneExiste(ligne)) {
+    		throw new IOException("la ligne que vous souhaité supprimer n'existe pas");
+    	
+    	}
+    	for(int indice = 0; indice<ligne ; indice++) {
+    		
+    		//TODO ecrire ici dans fichier bis avec la methode qui permettra d'ecrire
+    	}
+
+    }
+    public static void suppressionPlusieurLigne(String nomFichier , int ligneA , int ligneB) {
+    	//TODO ecrire la methode aprés suppression 1 ligne
+    }
+    /**
+     * methode qui permet d'ecrire une ligne de texte avant la ligne en paramétre
+     * @param ligne
+     * @throws IOException 
+     */
+    public static void ecrireLigneAvant(int ligne) throws IOException {
+    	String ecritFinal="";
+    	FileWriter fw = new FileWriter(fichier);
+    	BufferedWriter ecrire = new BufferedWriter(fw);
+    	for(int indice = 1 ; indice < ligne ; indice++) {
+    		ecrire.newLine();
+    	}
+    	for(int caractere = 0;caractere<75;caractere++) {
+    		ecritFinal += aEcrire.charAt(caractere);
+    	}
+    	ecrire.write(ecritFinal);
+    }
+    public static void main(String[] args) throws IOException {
+    	creationFichier(nomFichier);
+    	aEcrire ="fm,lgkjklfs,mu jigdf hogisf mhfo hfdljkh gnfh fb nlm h mjhqsdhny gcynfhsdk hbgjnjhfg ,sdg,l idf,kl j,n df kjgsn k: ryjndfh  bsrjhjetb u rlz rzklhrbjhbekjhhgdhg";
+    	ecrireLigneAvant(0);
+    	ouvrirFichier();
     }
 }
 
